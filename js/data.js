891f7a35799dbecc81d62b6eaaf75c20dfdd3150
Jack Woods <jackrwoods@gmail.com>
2018-03-29 10:37:10 -0700

    Add CSV parse functionality.

--- js/data.js
+++ js/data.js
@@ -1,48 +1,55 @@
 // Retrieve building data from server.
 
 var collectData = function collectData(obj) {
-  console.log(obj);
   obj = obj[0];
-
   // Perform linear search for building in currentData array.
   var i = 0;
   var found = false;
   while (!found) {
     if (currentData[i].id == obj.id) {
       found = true;
-      currentData[i].data.concat(obj.points);
+      currentData[i].data = obj.points;
     } else {
       i++;
     }
   }
-  console.log(currentData[i]);
 }
 // cauthorn, mcnary, sackett, west, wilson
 var currentData = [
   {
     name: "Cauthorn",
     id: "5aba8e95c224ce2b0eb05b1b", // Reference ObjectID
-    data: [] // Empty array that will be populated with data.
+    data: [], // Empty array that will be populated with data.
+    baseline: 0,
+    percentChange: 0
   },
   {
     name: "McNary",
     id: "5aba8eb6c224ce2b0eb05b1c",
-    data: []
+    data: [],
+    baseline: 0,
+    percentChange: 0
   },
   {
     name: "Sackett",
     id: "5aba8ec7c224ce2b0eb05b1d",
-    data: []
+    data: [],
+    baseline: 0,
+    percentChange: 0
   },
   {
     name: "West",
     id: "5aba8ed4c224ce2b0eb05b1e",
-    data: []
+    data: [],
+    baseline: 0,
+    percentChange: 0
   },
   {
     name: "Wilson",
     id: "5aba8ee5c224ce2b0eb05b1f",
-    data: []
+    data: [],
+    baseline: 0,
+    percentChange: 0
   }
 ];
 // Calculate this week's date range.
@@ -51,7 +58,7 @@ var day1 = new Date(d - 1000 * 60 * 60 * 24 * (d.getDay() - 1)); // Get date for
 /* d.getDay() - 1 returns monday as the first day of the week.
  * The baseline data starts on a Monday, so it's necessary.
  */
- 
+
 var startDate = day1.getFullYear() + "-" + ("0" + (day1.getMonth() + 1)).slice(-2) + "-" + ("0" + day1.getDate()).slice(-2);
 var currentDate = d.getFullYear() + "-" + ("0" + (d.getMonth() + 1)).slice(-2) + "-" + ("0" + (d.getDate() + 1)).slice(-2); // End date is not inclusive.
 
@@ -71,5 +78,28 @@ for (var i = 0; i < 5; i++) {
 }
 
 // Calculate percentage increase/decrease based on baseline data.
+// The function is called on an interval until all asynchronous data downloads are complete.
+var calcBaseline = setInterval(function() {
+  // If everything is downloaded...
+  if (
+    currentData[0].data.length != 0 &&
+    currentData[1].data.length != 0 &&
+    currentData[2].data.length != 0 &&
+    currentData[3].data.length != 0 &&
+    currentData[4].data.length != 0 &&
+    CSVList[0] != "" &&
+    CSVList[1] != "" &&
+    CSVList[2] != "" &&
+    CSVList[3] != "" &&
+    CSVList[4] != ""
+  ) {
+    clearInterval(calcBaseline); // Prevent the function from being called again.
+
+    for (var i = 0; i < 5; i++) {
+      currentData[i].baseline = getDataPoint(i, d) - getDataPoint(i, day1);
+    }
+  }
+
+}, 50); // Function is called every 50 milliseconds.
 
 // Display data on charts.
---
+++ js/parseCSV.js
@@ -0,0 +1,40 @@
+// Download & Parse CSV data files from server.
+var buildingList = [
+  "Cauthorn",
+  "McNary",
+  "Sackett",
+  "West",
+  "Wilson"
+];
+var CSVList = ["", "", "", "", ""]; // This will be populated with strings representing CSV files.
+                                    // CSVs are stored in the same order as buildingList.
+
+for (var i = 0; i < 5; i++) {
+  var xmlhttp;
+  if (window.XMLHttpRequest) {
+      // code for modern browsers
+      xmlhttp = new XMLHttpRequest();
+   } else {
+      // code for old IE browsers
+      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var data = this.responseText;
+      var url = this.responseURL;
+      if (url.indexOf("Cauthorn") != -1) {
+        CSVList[0] = Papa.parse(data);
+      } else if (url.indexOf("McNary") != -1) {
+        CSVList[1] = Papa.parse(data);
+      } else if (url.indexOf("Sackett") != -1) {
+        CSVList[2] = Papa.parse(data);
+      } else if (url.indexOf("West") != -1) {
+        CSVList[3] = Papa.parse(data);
+      } else {
+        CSVList[4] = Papa.parse(data);
+      }
+    }
+  };
+  xmlhttp.open("GET", "../data/" + buildingList[i] + ".csv", true);
+  xmlhttp.send();
+}
